6d55ce49c5ad2fd04899cd01f8c0a4dd4683b886
Yoshi Kiku <navi11ok22@gmail.com>
2019-05-04 23:12:15 +0900

    #7 csrfの設定

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') }}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
